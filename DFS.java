5627344db44ed7c34f0103bb3ba8dc5158e7fe5f
Murathan Akman <murathanakman@hotmail.com>
2022-12-15 23:58:25 +0300

    Update DFS.java
    -New ArrayList added which one is keeping single best node.
    
    -Did change FindBestSubOptimal Function type. Because this have to return a single Node.

--- DFS.java
+++ DFS.java
@@ -1,11 +1,14 @@
 import java.util.*;
+
 public class DFS {
   private ArrayList<Node> path = new ArrayList<Node>();
   private ArrayList<Node> frontier = new ArrayList<Node>();
   private ArrayList<Node> explored = new ArrayList<Node>(); // Expanded yap
   private ArrayList<Node> subOptimal = new ArrayList<Node>();
+  public ArrayList<Node> So = new ArrayList<>();
   // Inıtıal node added to frontier
   private int memorySize = 1;
+
   public DFS() {
 
   }
@@ -29,7 +32,7 @@ public class DFS {
   public ArrayList<Node> getSubOptimal() {
     return subOptimal;
   }
-  
+
   public void PathTrace(ArrayList<Node> path, Node node) {
     Node current = node;
     path.add(current);
@@ -39,70 +42,82 @@ public class DFS {
     }
   }
 
-  public ArrayList<Node> FindBestSubOptimal(ArrayList<Node> path){
+  public Node FindBestSubOptimal(ArrayList<Node> path) {
     int count = 0;
-    int best = 0;
-    for (int i = 0; i < path.size(); i++) {
-      Node current = subOptimal.get(i);
-      for(int j = 0; j < current.puzzle.length; j++){
-        if (current.puzzle[j] == 1){
+    int worst = 100; // Assume (does not imporant)
+    Node bestNode = null;
+    for (int i = 0; i < path.size(); i++) { // Traveling the path arraylist that given as parameter.
+      Node current = path.get(i); // Get the current node from the path arraylist -- current node will change
+                                  // respectively to the loop.
+      for (int j = 0; j < current.puzzle.length; j++) { // Traveling the puzzle array of the current node.
+        if (current.puzzle[j] == 1) { // If the current element of the puzzle array is 1, then increase the count.
           count++;
         }
       }
-      if(count > best){
-        best = count;
+      if (count < worst) {
+        worst = count; // If number of 1 of current node less than worst, assign new worst current
+                       // node's 1.
+        bestNode = current; // Then assign bestNode is current node.
       }
     }
-    return path;
+    return bestNode; // Return the bestNode.
   }
 
   public ArrayList<Node> Search(Node initNode) {
-      frontier.add(initNode);
-      boolean found = false;
-      while(frontier.size() > 0 && !found) {
-        
-          Node currentNode = frontier.get(frontier.size() - 1);
-          explored.add(currentNode);
-          frontier.remove(frontier.size() - 1);
-          currentNode.ExpandNode();
-          /*Print frontier queue (Sonradan silinecek)
-          currentNode.PrintPuzzle();
-          */
-          if(currentNode.children.size() == 0){
-              subOptimal.add(currentNode);
-          }
-          for (int i = 0; i < currentNode.children.size(); i++) {
-              Node currentChild = currentNode.children.get(i);
-              if (currentChild.GoalTest()) {
-              /* Print goal state if exists (Sonradan silinecek)
-              currentChild.PrintPuzzle();
-              */
-              found = true;
-              // Backtrack to find the path
+    frontier.add(initNode);
+    boolean found = false;
+    while (frontier.size() > 0 && !found) {
 
-              PathTrace(path, currentChild);
-              }
-
-              //Contains programın duplicate data yapmasının önüne geçmek için var
-              if (!Contains(frontier, currentChild) && !Contains(explored, currentChild)) {
-              frontier.add(currentChild);
-              memorySize++;
-              }
-          }     
+      Node currentNode = frontier.get(frontier.size() - 1);
+      explored.add(currentNode);
+      frontier.remove(frontier.size() - 1);
+      currentNode.ExpandNode();
+      /*
+       * Print frontier queue (Sonradan silinecek)
+       * currentNode.PrintPuzzle();
+       */
+      if (currentNode.children.size() == 0) {
+        subOptimal.add(currentNode);
       }
-    
-      // Reverse the path    
-      Collections.reverse(path);
-  return path;
+      for (int i = 0; i < currentNode.children.size(); i++) {
+        Node currentChild = currentNode.children.get(i);
+        if (currentChild.GoalTest()) {
+          /*
+           * Print goal state if exists (Sonradan silinecek)
+           * currentChild.PrintPuzzle();
+           */
+          found = true;
+          // Backtrack to find the path
+
+          PathTrace(path, currentChild);
+        }
+
+        // Contains programın duplicate data yapmasının önüne geçmek için var
+        if (!Contains(frontier, currentChild) && !Contains(explored, currentChild)) {
+          frontier.add(currentChild);
+          memorySize++;
+        }
+      }
+    }
+
+    // If goal state is not found, find the best sub-optimal path
+    if (found = false) {
+      So.add(FindBestSubOptimal(subOptimal)); // Add a bestNode (single one) to the So array which includes keeping a
+                                              // single best node.
+      return So;
+    } else {
+      Collections.reverse(path);// Reverse the path
+      return path;
+    }
   }
 
-  //Remove
+  // Remove
   public static boolean Contains(ArrayList<Node> frontier, Node node) {
     for (int i = 0; i < frontier.size(); i++) {
       if (frontier.get(i).IsSamePuzzle(node.puzzle)) {
         return true;
       }
-      
+
     }
     return false;
 
